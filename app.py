09f814bbf80b2b8b8e51df05d8fa9bc661e39588
Denis K <denis.kolosov@gmail.com>
2015-10-09 00:04:38 +0300

    Add form view and many to many relation

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask import render_template
 from sqlalchemy import create_engine
 from flask.ext.sqlalchemy import SQLAlchemy
+from wtforms import Form, StringField, IntegerField, validators
 
 
 app = Flask(__name__)
@@ -8,22 +10,41 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///marks.db'
 db = SQLAlchemy(app)
 db.create_all()
 
+student_to_discipline = db.Table('s_t_d',
+        db.Column('student_id', db.Integer, db.ForeignKey('student.id')),
+        db.Column('disipline_id', db.Integer, db.ForeignKey('discipline.id'))
+        )
 
+# MODELS
 class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
-    marks = db.relationship('Mark', backref='student', lazy='dynamic')
+    studying = db.relationship('Discipline',
+            secondary=student_to_discipline,
+            backref=db.backref('studying', lazy='dynamic'))
+    mark = db.Column(db.Integer)
 
     def __repr__(self):
         return 'Student {}'.format(self.name)
 
-
-class Mark(db.Model):
+class Discipline(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    discipline = db.Column(db.String(80))
-    mark = db.Column(db.Integer)
-    student_id = db.Column(db.Integer, db.ForeignKey('student.id'))
+    name = db.Column(db.String(80))
 
     def __repr__(self):
-        return 'Student {} got {} in {}'.format(self.student_id, self.mark,
-                                                self.discipline)
+        return self.name
+
+# VIEWS
+class StudentForm(Form):
+    name = StringField('Name', [validators.Length(max=120)])
+    discipline = StringField('Discipline', [validators.Length(max=80)])
+    mark = IntegerField('Mark', [validators.NumberRange(min=1, max=5)]) 
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = StudentForm()
+    return render_template('index.html', form=form)
+
+if __name__ == "__main__":
+    app.run(debug=True)
