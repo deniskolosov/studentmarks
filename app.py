9129fb401007c8fc80de31946417028710871bf1
Denis K <denis.kolosov@gmail.com>
2015-10-10 00:51:51 +0300

    Add styling

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ db.create_all()
 # MODELS
 class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120))
+    name = db.Column(db.String(120), unique=True)
 
     def __repr__(self):
         return 'Student {}'.format(self.name)
